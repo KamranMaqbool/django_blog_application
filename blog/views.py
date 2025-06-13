11da72d24ef77f8fde540740d4d5b402fa593902
kamran-pc <kamranmaqbool>
2025-06-13 16:13:34 +0500

    improve searching functionality with search rank

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,11 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+)
 
 from taggit.models import Tag
 
@@ -124,19 +128,23 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body')
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=query)
+                .filter(rank__gt=0.3)
+                .order_by('-rank')
             )
             
     return render(
         request,
         'blog/post/search.html',
         {
-        'form': form,
-        'query': query,
-        'results': results
+            'form': form,
+            'query': query,
+            'results': results
         }
     )
